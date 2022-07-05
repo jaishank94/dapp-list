cfb2711de2f56b25df1301f0cad5ac3e946c6912
lank <35355235+idnr@users.noreply.github.com>
2022-07-04 21:32:45 -0700

    navbar

---
+++ pages/components/navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+       <div>
+          <div>
+             <a href="/">Home</a>
+             <a href="/categories">categories</a>
+           </div>
+             <a href="/about">About</a>
+             <a href="/contact">Contact</a>
+             <a href="/privacy-policy">Privacy Policy</a>
+         </div>
+      )
+  }
