9d9cbd81be8358423f788d0b0df4d89540460ff5
lank <35355235+idnr@users.noreply.github.com>
2022-07-04 21:42:20 -0700

    navbarr

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Navbar from './components/navbar'
 
 export default function Home() {
   return (
